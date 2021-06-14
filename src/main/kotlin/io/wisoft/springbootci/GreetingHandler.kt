8b0ac04484ffe6ed4647173599f738c0935895dc
Daewon Park <20131828@edu.hanbat.ac.kr>
2021-06-14 18:00:30 +0900

    인사말 등록, 조회 기능 추가
    추가 사항:
    - 인사말 등록 기능 추가
    - 인사말 아이디로 조회 기능 추가
    - 인사말 전체 조회 기능 추가
    - 테스트 코드 추가
    
    Resolves: #1
    See also: None

--- src/main/kotlin/io/wisoft/springbootci/GreetingHandler.kt
+++ src/main/kotlin/io/wisoft/springbootci/GreetingHandler.kt
@@ -1,17 +1,23 @@
 package io.wisoft.springbootci
 
+import java.net.URI
 import java.util.concurrent.atomic.AtomicLong
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.badRequest
+import org.springframework.web.reactive.function.server.ServerResponse.created
+import org.springframework.web.reactive.function.server.ServerResponse.notFound
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.awaitBody
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import org.springframework.web.reactive.function.server.buildAndAwait
 import org.springframework.web.reactive.function.server.queryParamOrNull
 
 @Component
 class GreetingHandler {
-
     private val counter: AtomicLong = AtomicLong()
+    private var greetings = HashMap<Long, Greeting>()
 
     suspend fun greeting(request: ServerRequest): ServerResponse =
         ok()
@@ -21,4 +27,40 @@ class GreetingHandler {
                     "Hello, ${request.queryParamOrNull("name") ?: "World!"}"
                 )
             )
+
+    suspend fun create(request: ServerRequest): ServerResponse =
+        request
+            .awaitBody<Greeting>()
+            .let {
+                greetings[it.id] = it
+                created(URI.create("/greetings/${it.id}"))
+                    .buildAndAwait()
+            }
+
+    suspend fun getAll(request: ServerRequest): ServerResponse =
+        ok()
+            .bodyValueAndAwait(greetings.values)
+
+    suspend fun getById(request: ServerRequest): ServerResponse {
+        val id = request
+            .pathVariable("id")
+            .toLongOrNull()
+            ?: return badRequest()
+                .buildAndAwait()
+
+        return greetings[id]
+            ?.let {
+                ok()
+                    .bodyValueAndAwait(it)
+            } ?: notFound()
+            .buildAndAwait()
+    }
+
+    fun initialize() {
+        this.greetings = hashMapOf(
+            1L to Greeting(1, "Hello, World!"),
+            2L to Greeting(2, "Hello, Sangmin!"),
+            3L to Greeting(3, "Hello, Soonho!"),
+        )
+    }
 }
--- src/test/kotlin/io/wisoft/springbootci/GreetingRoutesTests.kt
+++ src/test/kotlin/io/wisoft/springbootci/GreetingRoutesTests.kt
@@ -1,13 +1,23 @@
 package io.wisoft.springbootci
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.test.web.reactive.server.expectBodyList
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GreetingRoutesTests(@Autowired val client: WebTestClient) {
+    @Autowired
+    val greetingHandler = GreetingHandler()
+
+    @BeforeEach
+    fun initialize() =
+        greetingHandler.initialize()
+
     @Test
     fun `기본 인사말 테스트`() {
         client
@@ -31,4 +41,41 @@ class GreetingRoutesTests(@Autowired val client: WebTestClient) {
             .jsonPath("content")
             .isEqualTo("Hello, Daewon")
     }
+
+    @Test
+    fun `새로운 인사말 등록`() {
+        client
+            .post()
+            .uri("/greetings")
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .bodyValue(Greeting(4, "Hello, Daewon!"))
+            .exchange()
+            .expectStatus().isCreated
+            .expectHeader()
+            .location("/greetings/4")
+    }
+
+    @Test
+    fun `아이디로 인사말 조회`() {
+        client
+            .get()
+            .uri("/greetings/1")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("content")
+            .isEqualTo("Hello, World!")
+    }
+
+    @Test
+    fun `인사말 전체 조회`() {
+        client
+            .get()
+            .uri("/greetings")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList<Greeting>()
+            .hasSize(3)
+    }
+
 }
